e41ca7867352102787ebb3d83ea762792359e7ca
thespud56 <stu.a.murphy@gmail.com>
2020-01-17 13:23:51 -0500

    adding function

---
+++ .ipynb_checkpoints/functions-checkpoint.py
@@ -0,0 +1,17 @@
+#  Import statements
+import pandas as pd
+import numpy as np
+import missingno
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+from IPython.display import display_html
+
+
+def clean_df(raw_df):
+    '''Function that removes duplicate wines, unwanted columns, naans, and wines above $100'''
+    nodup_df = raw_df.duplicated(subset='title', keep=False)
+    nodup_df = raw_df[nodup_df].drop(['Unnamed: 0'], axis=1)
+    nodup_df = nodup_df.dropna()
+    nodup_df.drop(df, inplace=True)
+    return nodup_df
