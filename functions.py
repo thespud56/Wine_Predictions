1fd80d3340d5f537bce87a4752a7d7a6d59b8f76
thespud56 <stu.a.murphy@gmail.com>
2020-01-21 17:41:35 -0500

    yo

--- functions.py
+++ functions.py
@@ -11,6 +11,7 @@ from IPython.display import display_html
 def clean_df(raw_df):
     '''Function that removes duplicate wines, unwanted columns, naans, and wines above $100'''
     nodup_df = raw_df.drop_duplicates(subset='title', keep='first')
+    nodup_df = nodup_df.drop(['region_1', 'region_2'], axis=1)
     nodup_df = nodup_df.dropna()
     df = nodup_df[nodup_df['price'] >= 300].index
     nodup_df = nodup_df.drop(df, inplace=True)
